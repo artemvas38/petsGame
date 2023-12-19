c51602be90b6a985ebd10f0a86e6251e56f9a2d2
artemvas38 <artemvas38123@yandex.ru>
2023-12-19 14:23:20 +0300

    0.0.3 add "advertisement" and Q/A

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,15 @@ let petName = document.getElementById(`pet_name`);
 let food1 = document.getElementById(`food1`);
 let food2 = document.getElementById(`food2`);
 
+let food1Text = document.getElementById(`food1Text`);
+let food2Text = document.getElementById(`food2Text`);
+
+// adversiment
+
+let adNode = document.querySelector(`.ad`);
+let adSVG = document.querySelector(`.ad_svg`);
+
+
 let money = 100;
 let petStarvation = 30;
 
@@ -14,7 +23,12 @@ const food2Cost = 50;
 const food1Saturation = 30;
 const food2Saturation = 50;
 
+
+
 let spendHungerTimer;
+let adTimer;
+
+let adTime = 5;
 
 (function spendHungerTimerFunc() {
     petStarvation--;
@@ -29,19 +43,46 @@ let spendHungerTimer;
     }
 }())
 
+function adTimerFunc() {
+    adTime--;
+    if (adTime < 0) {
+        clearTimeout(adTimer);
+        adNode.style.display = `none`;
+    }
+    else {
+        timer = setTimeout(adTimerFunc, 1000);
+        if (adTime <= 3) {
+            adSVG.style.display = `inline`;
+        }
+    }
+}
+
+
+
 
 food1.addEventListener(`click`, function () {
     if ((money >= food1Cost) && ((petStarvation + food1Saturation) <= 100)) {
+        adTimerFunc()
+        adNode.style.display = `flex`;
         money -= food1Cost;
         petStarvation += food1Saturation;
+
+
+
     }
 })
 
 food2.addEventListener(`click`, function () {
     if ((money >= food2Cost) && ((petStarvation + food2Saturation) <= 100)) {
+        adTimerFunc()
+        adNode.style.display = `flex`;
         money -= food2Cost;
         petStarvation += food2Saturation;
     }
 })
+
+food1Text.innerHTML = food1Cost;
+food2Text.innerHTML = food2Cost;
+
 
 
